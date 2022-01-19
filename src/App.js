d2ba754c576758d33eed408d43c384d7a6ed44d5
SE404 <sergey.f.emelyanov@gmail.com>
2022-01-19 23:45:21 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import PersonalDetailsForm from "./components/PersonalDetailsForm";
+
+function App() {
+  return (
+    <div>
+      <PersonalDetailsForm></PersonalDetailsForm>
+    </div>
+  );
+}
+
+export default App;
